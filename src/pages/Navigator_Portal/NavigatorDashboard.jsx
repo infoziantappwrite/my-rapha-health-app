7d767504e15bf5624243fb7425da33f345ac0c77
Harirajhari <hariraj121203@gmail.com>
2025-09-15 17:26:44 +0530

    chnage the schedule compoent

--- src/pages/Navigator_Portal/NavigatorDashboard.jsx
+++ src/pages/Navigator_Portal/NavigatorDashboard.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Navigator_Workflow } from './compoents/Navigator_Workflow'
-import Highlight from './compoents/Highlight'
-import Table from './compoents/Table'
-import NavigatorTools from './compoents/NavigatorTools'
-
+import React from "react";
+import { Navigator_Workflow } from "./compoents/Navigator_Workflow";
+import Highlight from "./compoents/Highlight";
+import Table from "./compoents/Table";
+import NavigatorTools from "./compoents/NavigatorTools";
 
 const NavigatorDashboard = () => {
   return (
-    <div className="relative">
-      {/* Sticky workflow */}
-      <div className="sticky top-0 z-50">
-        <Navigator_Workflow />
+    <div className="relative px-4 md:px-6 lg:px-8 space-y-6">
+      {/* Workflow Timeline */}
+      <div className="md:sticky md:top-0 md:z-50">
+        {/* On mobile, allow horizontal scrolling for steps */}
+        <div className="overflow-x-auto md:overflow-visible">
+          <div className="min-w-[600px] md:min-w-0">
+            <Navigator_Workflow />
+          </div>
+        </div>
       </div>
 
-      {/* Scrollable highlights */}
-      <div className="mt-6">
+      {/* Main content */}
+      <div className="space-y-6">
         <Highlight />
         <Table />
         <NavigatorTools />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavigatorDashboard
+export default NavigatorDashboard;
